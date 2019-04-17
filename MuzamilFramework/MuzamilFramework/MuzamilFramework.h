d9e887cd10f7d52377681aeaec290eaca1f7555e
Muzamil Hassan <muzamilhassan@axact.com>
2019-04-17 12:57:19 +0500

    Local framework

---
+++ MuzamilFramework/MuzamilFramework/MuzamilFramework.h
@@ -0,0 +1,19 @@
+//
+//  MuzamilFramework.h
+//  MuzamilFramework
+//
+//  Created by muzamilhassan on 17/04/2019.
+//  Copyright © 2019 muzamilhassan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MuzamilFramework.
+FOUNDATION_EXPORT double MuzamilFrameworkVersionNumber;
+
+//! Project version string for MuzamilFramework.
+FOUNDATION_EXPORT const unsigned char MuzamilFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MuzamilFramework/PublicHeader.h>
+
+
---
+++ MuzamilFramework/MuzamilFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  MuzamilFramework
+//
+//  Created by muzamilhassan on 17/04/2019.
+//  Copyright © 2019 muzamilhassan. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {
+        
+    }
+    public static func doSomething() ->  String {
+        return "Do some thing"
+    }
+}
